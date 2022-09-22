c44bb2dd8ef493ef6e81aa733bebe0f3bb5ba3e2
Abel Alvarez <learn.belbel@gmail.com>
2022-09-21 19:32:04 -0600

    first commint

---
+++ src/com/ejecicio/Main.java
@@ -0,0 +1,36 @@
+package com.ejecicio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // numericos
+        // Enteros
+
+        int x = 5;
+        System.out.println(x);
+        long x2 = 5;
+        System.out.println(x2);
+        byte x3 = 2;
+        System.out.println(x3);
+        short x4 = 3;
+        System.out.println(x3);
+
+        // Decimales
+        float x5 = 1.0f;
+        System.out.println(x5);
+        double x6 = 1.0;
+        System.out.println(x6);
+
+        //booleano
+        boolean esdatonumerox4 = true;
+        System.out.println(esdatonumerox4);
+        boolean esdatodetextox3 = false;
+
+        //texto
+        char y =  'y';
+        System.out.println(y);
+        String texto = "esto es un texto";
+    }
+}
+
